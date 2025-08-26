5a29f59b63fc65036c8a9aee90cfe24cb58f4f43
mileesonani <mileesonani@gmail.com>
2025-08-26 18:08:03 +0530

    Google analitic added

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,8 @@
 'use client'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import * as gtag from '../lib/gtag';
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Experience from "../components/Experience";
@@ -9,8 +13,38 @@ import Skills from "../components/Skills";
 import { SnackbarProvider } from 'notistack';
 
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
       <main className="text-font_color sm:overflow-x-visible overflow-x-hidden">
         <div className="fixed w-[100%] z-[1000] bg-primary">
           <Header />
