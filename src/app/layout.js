0e1bfd36f6a954ab6bfe875676ba57bfab1ce570
mileesonani <mileesonani@gmail.com>
2025-08-26 18:16:51 +0530

    Changes for Google analitic

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 // import "./globals.css";
 import "./global_dark.css";
 import Script from "next/script";
+import * as gtag from '../lib/gtag';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -68,6 +69,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+            `,
+        }}
+      />
       <Script id="ld-json" type="application/ld+json">
         {JSON.stringify({
           "@context": "https://schema.org",
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,4 @@
 'use client'
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
-import * as gtag from '../lib/gtag';
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Experience from "../components/Experience";
@@ -13,38 +9,8 @@ import Skills from "../components/Skills";
 import { SnackbarProvider } from 'notistack';
 
 export default function Home() {
-
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
-      {/* Google Analytics Script */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_MEASUREMENT_ID}');
-          `,
-        }}
-      />
       <main className="text-font_color sm:overflow-x-visible overflow-x-hidden">
         <div className="fixed w-[100%] z-[1000] bg-primary">
           <Header />
