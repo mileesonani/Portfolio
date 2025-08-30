5069710f74d04a7678433c08f45c2cb949d8f8d6
mileesonani <mileesonani@gmail.com>
2025-08-30 17:51:21 +0530

    Changes in variable name

--- src/app/layout.js
+++ src/app/layout.js
@@ -46,7 +46,7 @@ export const metadata = {
     icon: "/assets/Milee2.jpg",
   },
   verification: {
-    google: process.env.VERIFICATION_CODE
+    google: process.env.NEXT_PUBLIC_VERIFICATION_CODE
   }
 };
 
@@ -55,7 +55,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
       />
       <Script
         // id="gtag-init"
@@ -66,7 +66,7 @@ export default function RootLayout({ children }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.GA_MEASUREMENT_ID}');
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
             `,
         }}
       />
